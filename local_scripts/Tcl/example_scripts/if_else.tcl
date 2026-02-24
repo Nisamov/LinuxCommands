5e5b7537ff71d7bc3355d716c966442426577ae1
Nisamov <aaa.002.0a0b@gmail.com>
2026-02-24 18:44:43 +0100

    Contenido agregado lenguaje Tcl

---
+++ local_scripts/Tcl/example_scripts/if_else.tcl
@@ -0,0 +1,9 @@
+# Definir la variable correctamente
+set num 1
+
+# Condicional
+if {$num > 0} {
+    puts "Numero mayor que 0 = $num"
+} else {
+    puts "Numero menor o igual a 0 = $num"
+}
---
+++ local_scripts/Tcl/example_scripts/procedures.tcl
@@ -0,0 +1,10 @@
+# Una función (procedimiento)
+set contenido "parametro"
+
+#Proceso de accion
+proc nombre_proc {nombre} {
+    puts "Ejemplo, $nombre"
+}
+
+# Llamar al procedimiento
+nombre_proc $contenido
